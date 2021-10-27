6a37e067dbef583e2f34d194652c8ba45c1e29a7
LubosSukup <Lubos.Sukup@kyndryl.com>
2021-10-27 12:54:47 +0200

    43 tyzden if else

--- src/main/java/prvypolrok/tyzden42/VetvenieKodu.java
+++ src/main/java/prvypolrok/tyzden42/VetvenieKodu.java
@@ -4,7 +4,13 @@ public class VetvenieKodu {
 
     public static void main(String[] args) {
 
-        dlzkaMena("Lubos Sukup");
+       // dlzkaMena("Lubos Sukup");
+        System.out.println("===========len If ==================");
+        lenIf(true,"Lubos");
+        lenIf(false,"Lubos");
+        System.out.println("=========== if else ==================");
+        ifElse(true,"Lubos");
+        ifElse(false,"Lubos");
     }
 
     public static void dlzkaMena(String meno){
@@ -14,7 +20,31 @@ public class VetvenieKodu {
         }else {
             System.out.println("moje meno je mensie ako 15 znakov");
         }
-
     }
 
+
+    public static void lenIf(boolean vieJavu, String meno){
+        if(vieJavu){
+            meno = meno + " vie Javu";
+        }
+        meno = meno + " vie matematiku";
+        System.out.println(meno);
+    }
+    public static void ifElse(boolean vieJavu, String meno){
+        if(vieJavu){
+            meno = meno + " vie Javu";
+        }else {
+            meno = meno + " vie matematiku";
+        }
+        System.out.println(meno);
+    }
+
+    public static void dniVtyzdni(int cislo){
+
+        if(cislo == 1){
+            System.out.println("Pondelok");
+        }else if (cislo == 2){
+            System.out.println("Utorok");
+        }/// a doplnime
+    }
 }
--- src/main/java/prvypolrok/ulohy/Test.java
+++ src/main/java/prvypolrok/ulohy/Test.java
@@ -5,109 +5,22 @@ import java.util.*;
 public class Test {
 
     public static void main(String[] args) {
-        Set<Integer> ttest = new SortedSet<Integer>() {
-            @Override
-            public Comparator<? super Integer> comparator() {
-                return null;
-            }
+       // System.out.println( scitanie1());
+        System.out.println(scitanie2(2,2));
+    }
 
-            @Override
-            public SortedSet<Integer> subSet(Integer fromElement, Integer toElement) {
-                return null;
-            }
+    static void scitanie(){
+        int x = 1 +1;
+    }
 
-            @Override
-            public SortedSet<Integer> headSet(Integer toElement) {
-                return null;
-            }
+    static int scitanie1(){
 
-            @Override
-            public SortedSet<Integer> tailSet(Integer fromElement) {
-                return null;
-            }
+            int x = 1 +1;
+       return x;
+    }
 
-            @Override
-            public Integer first() {
-                return null;
-            }
-
-            @Override
-            public Integer last() {
-                return null;
-            }
-
-            @Override
-            public int size() {
-                return 0;
-            }
-
-            @Override
-            public boolean isEmpty() {
-                return false;
-            }
-
-            @Override
-            public boolean contains(Object o) {
-                return false;
-            }
-
-            @Override
-            public Iterator<Integer> iterator() {
-                return null;
-            }
-
-            @Override
-            public Object[] toArray() {
-                return new Object[0];
-            }
-
-            @Override
-            public <T> T[] toArray(T[] a) {
-                return null;
-            }
-
-            @Override
-            public boolean add(Integer integer) {
-                return false;
-            }
-
-            @Override
-            public boolean remove(Object o) {
-                return false;
-            }
-
-            @Override
-            public boolean containsAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean addAll(Collection<? extends Integer> c) {
-                return false;
-            }
-
-            @Override
-            public boolean retainAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public boolean removeAll(Collection<?> c) {
-                return false;
-            }
-
-            @Override
-            public void clear() {
-
-            }
-        };
-
-        ttest.add(300);
-        //ttest.add(40);
-        ttest.add(1);
-        ttest.add(2);
-        ttest.add(50);
-
-        System.out.println(ttest);
+    static int scitanie2(int scitanec1 , int scitanec2){
+        int vysledok    = scitanec1 +scitanec2;
+        return vysledok;
     }
 }
